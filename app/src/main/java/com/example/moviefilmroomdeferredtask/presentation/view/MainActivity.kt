5ed118962cf66face99dfe135e3296e1bd67b739
Sergey Belov <sergpc@mail.ru>
2020-09-02 23:38:14 +0300

    Работа над ошибками

--- app/src/main/java/com/example/moviefilmroomdeferredtask/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/moviefilmroomdeferredtask/presentation/view/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity(), MovieListFragment.MovieListListener,
     //val movieViewModel : MovieListViewModel? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         //val Test = intent.getStringExtra("showSeeLate")
-        val idMoveSeeLate = getIntent().getStringExtra("nameMovie")
+        val idMoveSeeLate = getIntent().getStringExtra("idMovie")
 
 
         super.onCreate(savedInstanceState)
